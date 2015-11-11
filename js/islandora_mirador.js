510cb77722e8db53fa91b571839e99a92d5b75e5
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2015-11-11 17:55:45 -0500

    Added fields to enter IIIF server url and manifest ds.
    The IIIF server will supply the viewer with transformed images using IIIF standard.

--- js/islandora_mirador.js
+++ js/islandora_mirador.js
@@ -1,4 +1,12 @@
 $(function() {
+	/*
+	var pid = Drupal.settings.islandora_mirador_bookreader.pid;
+	console.log(pid);
+	var url = "https://sz-paleographydev.library.utoronto.ca/islandora/object/";
+	url += pid  + "/datastream/SC/view"; 
+	console.log(url);
+	*/
+
       Mirador({
         "id": "mirador-reader", // The CSS ID selector for the containing element.
         "layout": "1x1", // The number and arrangement of windows ("row"x"column")?
@@ -6,20 +14,18 @@ $(function() {
         // This array holds the manifest URIs for the IIIF resources you want Mirador to make available to the user.
         // Each manifest object must have a manifest URI pointing to a valid IIIF manifest, and may also
         // provide a location to be displayed in the listing of available manifests.
-          { "manifestUri": "https://content.library.utoronto.ca//paleography/mirador/manifest.json", "location": "Stanford University"}
+          { "manifestUri": "https://content.library.utoronto.ca/paleography/mirador/manifest.json", "location": "University of Toronto"}
+        //  { "manifestUri": url, "location": "University of Toronto Libraries"}
         ],
         // This array allows the user to specify which of the included manifests should appear
         // in the workspace, and what the configuration of the window (zoom level, open panels, etc.)
         // ought to be. To begin with, we will leave it blank.
         "windowObjects": [],
 	'mainMenuSettings': {
-	      'show': true,
 	      'buttons' : {
-		'bookmark' : false,
-		'layout' : true,
-		'options' : false,
-		'fullScreenViewer': true
+		'bookmark' : false
 	      }
 	}
       });
-    });
+	
+});
