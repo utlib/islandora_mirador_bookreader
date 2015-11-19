7baa8a774292cb95402d35d9a511c4514435408f
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2015-11-19 15:07:16 -0500

    Completed COL-617
    The view can now be configured through islandora/islandora_viewers/ to changed the default view to either Image, Book or Thumbnail. The labels and options can be easily changed in the islandora_mirador.js in the module.

--- js/islandora_mirador.js
+++ js/islandora_mirador.js
@@ -1,12 +1,25 @@
 $(function() {
 	var pid = Drupal.settings.islandora_mirador_bookreader.pid;
-	console.log(">>>>" + pid + "<<<<");
-	var manifest_url = "https://sz-paleographydev.library.utoronto.ca/islandora/object/" + pid + "/datastream/SC/";	
-	console.log(manifest_url);
+	var view_type = Drupal.settings.islandora_mirador_bookreader.view_type;	
+	switch(parseInt(view_type)) {
+		case 1:
+			view_type = "ImageView";
+			break;
+		case 2:	
+			view_type = "BookView";
+			break;	
+		case 3:
+			view_type = "ThumbnailsView";
+			break;
+		default:
+			view_type = "ImageView";
+	}
+	console.log("The view type from config is " + view_type + " <<<<");
+	var manifest_url = "https://paleography.library.utoronto.ca/islandora/object/" + pid + "/datastream/SC/";	
 
       Mirador({
         "id": "mirador-bookreader", // The CSS ID selector for the containing element.
-        "layout": "1x1", // The number and arrangement of windows ("row"x"column")?
+        //"layout": "1x1", // The number and arrangement of windows ("row"x"column")?	
         "data": [ 
         // This array holds the manifest URIs for the IIIF resources you want Mirador to make available to the user.
         // Each manifest object must have a manifest URI pointing to a valid IIIF manifest, and may also
@@ -14,20 +27,21 @@ $(function() {
         { "manifestUri": manifest_url, "location": "University of Toronto"}
         //{ "manifestUri": "https://sz-paleographydev.library.utoronto.ca/sites/sz-paleographydev.library.utoronto.ca/files/json/toronto.json", "location": "University of Toronto"}
         ],
-        // This array allows the user to specify which of the included manifests should appear 
-        // in the workspace, and what the configuration of the window (zoom level, open panels, etc.) 
-        // ought to be. To begin with, we will leave it blank. 
 	'mainMenuSettings': {
-              'buttons' : {
-                'bookmark' : false
-              }
-        },
+		'show':false,
+		'buttons':{
+			'bookmark' : false
+		}
+	},
 	"showAddFromURLBox":false,	
         "windowObjects": [
 		{
 			"loadedManifest":manifest_url,
-			"viewType":"ImageView",
-			"annotationLayer":false
+			"viewType":view_type,
+			"annotationLayer":false,
+			"displayLayout": false,
+			"bottomPanel" : true,
+			"sidePanel" : false
 		}
 	]
       });
