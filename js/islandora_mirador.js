2d13e85b0256f106110af4e5b03d88670cb69ab3
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2015-11-30 16:21:16 -0500

    Added documentation to main javascript
    Removed comment and test lines, and added short descriptions for each part.

--- js/islandora_mirador.js
+++ js/islandora_mirador.js
@@ -1,5 +1,5 @@
 $(function() {
-	var pid = Drupal.settings.islandora_mirador_bookreader.pid;
+	// This view type corresponds with the Drupal module setting
 	var view_type = Drupal.settings.islandora_mirador_bookreader.view_type;	
 	switch(parseInt(view_type)) {
 		case 1:
@@ -15,15 +15,15 @@ $(function() {
 			view_type = "ImageView";
 	}
 	//console.log("The view type from config is " + view_type + " <<<<");
+
+	// The manifest_url is dynamically generated based on the pid
+	var pid = Drupal.settings.islandora_mirador_bookreader.pid;
 	var manifest_url = "https://paleographydev.library.utoronto.ca/islandora/object/" + pid + "/datastream/SC/";	
 
+	// This is the main Mirador constructor. The settings corresponds to settings.js in the source code, see there for more help
       Mirador({
-        "id": "mirador-bookreader", // The CSS ID selector for the containing element.
-        //"layout": "1x1", // The number and arrangement of windows ("row"x"column")?	
+        "id": "mirador-bookreader",
         "data": [ 
-        // This array holds the manifest URIs for the IIIF resources you want Mirador to make available to the user.
-        // Each manifest object must have a manifest URI pointing to a valid IIIF manifest, and may also
-        // provide a location to be displayed in the listing of available manifests.
         { "manifestUri": manifest_url, "location": "University of Toronto"}
         //{ "manifestUri": "https://sz-paleographydev.library.utoronto.ca/sites/sz-paleographydev.library.utoronto.ca/files/json/toronto.json", "location": "University of Toronto"}
         ],
@@ -61,5 +61,6 @@ $(function() {
 	]
       });
 	
+	// Mirador loads its own jQuery version (1.9.1). We need to raise Drupal's jQuery version using the migrate library, if Drupal's behind
 	$.getScript("https://code.jquery.com/jquery-migrate-1.2.1.js");
 });
