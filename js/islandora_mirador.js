f5aa01724eedc5949e5822b3853544506fd222fa
Chul Hee Yoon <yoonc@dcollections.library.utoronto.ca>
2015-12-02 15:26:13 -0500

    COL-518
    base_url instead of hardcoded url

--- js/islandora_mirador.js
+++ js/islandora_mirador.js
@@ -21,8 +21,10 @@ $(function() {
 
 	var manifest_list =JSON.parse(Drupal.settings.islandora_mirador_bookreader.sc_manifest_list);
 
+	var base_url = Drupal.settings.islandora_mirador_bookreader.base_url;
+
 	//put a base url
-	var manifest_url = "https://cy-paleographydev.library.utoronto.ca/islandora/object/" + pid + "/datastream/SC/";	
+	var manifest_url = base_url + "/islandora/object/" + pid + "/datastream/SC/";	
 
 	// This is the main Mirador constructor. The settings corresponds to settings.js in the source code, see there for more help
       Mirador({
