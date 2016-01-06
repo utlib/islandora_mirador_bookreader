828722b6fb1ff73afec44702ef478f49af185efe
Sunny Lee <sunnywd.lee@utoronto.ca>
2016-01-06 18:17:19 -0500

    COL-737: loads the current manifest first
    https://support.library.utoronto.ca/browse/COL-737

--- js/islandora_mirador.js
+++ js/islandora_mirador.js
@@ -16,10 +16,12 @@ $(function() {
 			view_type = "ImageView";
 	}
 	//console.log("The view type from config is " + view_type + " <<<<");
+	
 
 	var json_path = Drupal.settings.islandora_mirador_bookreader.json_file_directory+'sc_manifest_list.json';
 
 	$.getJSON(json_path, function(data) {
+
 
 	// The manifest_url is dynamically generated based on the pid
 	var pid = Drupal.settings.islandora_mirador_bookreader.pid;
@@ -29,47 +31,65 @@ $(function() {
 	//put a base url
 	var manifest_url = base_url + "/islandora/object/" + pid + "/datastream/SC/view";	
 
+	var current_manifest_url = base_url + "/islandora/object/" + current_pid + "/datastream/SC/view"; 
+
+	// Get the current pid from the url
+	var pathname = window.location.pathname;
+		pathname = pathname.split("/");
+	var current_pid = pathname[3];
+		current_pid = current_pid.replace('paleography%3A', 'paleography:');
+
+	// Create a new data array
+	var new_data = []; 
+
+	// Grab the current object and put it into the beginning of the array
+	// This is to make the Mirador viewer load faster
+
+	$.each(data, function(idx, obj) {
+		if(obj.manifestUri == current_manifest_url) { 
+			new_data.unshift(obj); 
+		} else { new_data.push(obj); }
+	});
+
 	// This is the main Mirador constructor. The settings corresponds to settings.js in the source code, see there for more help
 	
-      Mirador({
-        "id": "mirador-bookreader",
-        "data": data,
-	//"data":[{ "manifestUri": manifest_url, "location": "University of Toronto"}],
-	'mainMenuSettings': {
-		'show':false,
-		'buttons':{
-			'bookmark' : false
-		}
-	},
-	"showAddFromURLBox":false,	
+		Mirador({
+			"id": "mirador-bookreader",
+			"data": new_data,
+			//"data":[{ "manifestUri": manifest_url, "location": "University of Toronto"}],
+			'mainMenuSettings': {
+				'show':false,
+				'buttons': { 'bookmark' : false }
+			},
+			"showAddFromURLBox":false,	
 
-	'buildPath' : '/sites/all/libraries/mirador/',
+			'buildPath' : '/sites/all/libraries/mirador/',
 
-	'i18nPath' : '/sites/all/libraries/mirador/locales/',
+			'i18nPath' : '/sites/all/libraries/mirador/locales/',
 
-	'imagesPath' : '/sites/all/libraries/mirador/images/',
+			'imagesPath' : '/sites/all/libraries/mirador/images/',
 
-	'logosPath' : '/sites/all/libraries/mirador/images/logos/',
-	
-	'logosLocation' : '/sites/all/libraries/mirador/images/logos/',
+			'logosPath' : '/sites/all/libraries/mirador/images/logos/',
 
-	'repoImages' : {
-		'other': 'iiif_logo.png'
-	},
+			'logosLocation' : '/sites/all/libraries/mirador/images/logos/',
 
-        "windowObjects": [
-		{
-			"loadedManifest":manifest_url,
-			"viewType":view_type,
-			"annotationLayer":false,
-			"displayLayout": true,
-			"bottomPanel" : true,
-			"sidePanel" : false,
-			"overlayMetadata": false
-		}
-	]
-      });
-	});
+			'repoImages' : {
+				'other': 'iiif_logo.png'
+			},
+
+			"windowObjects": [
+				{
+					"loadedManifest":manifest_url,
+					"viewType":view_type,
+					"annotationLayer":false,
+					"displayLayout": true,
+					"bottomPanel" : true,
+					"sidePanel" : false,
+					"overlayMetadata": false
+				}
+			]
+		});
+	}); //getJSON
 
 });
 
