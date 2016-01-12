f93974fa828edf1c998691413b0ce2f6d95e3b77
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2016-01-12 18:21:54 -0500

    Added manifest datastream ID option~

--- js/islandora_mirador.js
+++ js/islandora_mirador.js
@@ -20,8 +20,8 @@ $(function() {
 	/* Setup the base URL for windowObjects or data attribute to render */
 	var pid = Drupal.settings.islandora_mirador_bookreader.pid;
 	var base_url = Drupal.settings.islandora_mirador_bookreader.base_url;
-	var manifest_datastream = Drupal.settings.islandora_mirador_bookreader.manifest_datastream;
-	var manifest_url = base_url + "/islandora/object/" + pid + "/datastream/" + manifest_datastream + "/view";	
+	var manifest_datastream_id = Drupal.settings.islandora_mirador_bookreader.manifest_datastream_id;
+	var manifest_url = base_url + "/islandora/object/" + pid + "/datastream/" + manifest_datastream_id + "/view";	
 
 	/* Retrieve the JSON array of links to manifests, if any, and supply it to Mirador's constructor */
 	var manifest_list_url = Drupal.settings.islandora_mirador_bookreader.manifest_list_url;
