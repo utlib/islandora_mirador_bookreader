1799b748f3865ce88e59240e6911d66e31e62b13
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2016-01-12 18:15:30 -0500

    Added no manifest list option to initialization script

--- js/islandora_mirador.js
+++ js/islandora_mirador.js
@@ -1,5 +1,7 @@
 (function($) {
 $(function() {
+
+	/* Switch the Mirador view type based on the Drupal setting */
 	var view_type = Drupal.settings.islandora_mirador_bookreader.view_type;	
 	switch(parseInt(view_type)) {
 		case 1:
@@ -15,71 +17,71 @@ $(function() {
 			view_type = "ImageView";
 	}
 
+	/* Setup the base URL for windowObjects or data attribute to render */
+	var pid = Drupal.settings.islandora_mirador_bookreader.pid;
+	var base_url = Drupal.settings.islandora_mirador_bookreader.base_url;
+	var manifest_datastream = Drupal.settings.islandora_mirador_bookreader.manifest_datastream;
+	var manifest_url = base_url + "/islandora/object/" + pid + "/datastream/" + manifest_datastream + "/view";	
+
+	/* Retrieve the JSON array of links to manifests, if any, and supply it to Mirador's constructor */
 	var manifest_list_url = Drupal.settings.islandora_mirador_bookreader.manifest_list_url;
 	if (!manifest_list_url) {
-		manifest_list_url = Drupal.settings.islandora_mirador_bookreader.json_file_directory+'sc_manifest_list.json';
-	}
-
-	$.getJSON(manifest_list_url, function(data) {
-
-		// Retrieves the current object's PID and uses it to construct the URL for Mirador's windowObjects' loadedManifest attribute 
-		var pid = Drupal.settings.islandora_mirador_bookreader.pid;
-		var base_url = Drupal.settings.islandora_mirador_bookreader.base_url;
-		var manifest_url = base_url + "/islandora/object/" + pid + "/datastream/SC/view";
-			
-		// Grab the current object and put it into the beginning of the array
-		// This is to make the Mirador viewer load faster
-		var new_data = []; 
-		$.each(data, function(idx, obj) {
-			if (obj.manifestUri == manifest_url) {
-				new_data.unshift(obj); 
-			} else {
-				new_data.push(obj); 
-			}
-		});
 
 		Mirador({
 			"id": "mirador-bookreader",
-			"data": new_data,
-			
-			/* Optional parameters
 			"data":[{ "manifestUri": manifest_url, "location": "University of Toronto"}],
-			'mainMenuSettings': {
-				'show':false,
-				'buttons': { 'bookmark' : false }
-			},
-			"showAddFromURLBox":false,	
-			*/
-
 			'buildPath' : '/sites/all/libraries/mirador/',
-
 			'i18nPath' : '/sites/all/libraries/mirador/locales/',
-
 			'imagesPath' : '/sites/all/libraries/mirador/images/',
-
 			'logosPath' : '/sites/all/libraries/mirador/images/logos/',
-
 			'logosLocation' : '/sites/all/libraries/mirador/images/logos/',
-
 			'repoImages' : {
 				'other': 'iiif_logo.png'
 			},
-
 			"windowObjects": [
 				{
-					"loadedManifest":manifest_url,
-					"viewType":view_type
-					/*
-					"annotationLayer":false,
-					"displayLayout": true,
-					"bottomPanel" : true,
-					"sidePanel" : false,
-					"overlayMetadata": false
-					*/
-				}
-			]
+                                	"loadedManifest":manifest_url,
+                                }
+			]		
 		});
-	}); //getJSON
+
+	} else {
+
+		$.getJSON(manifest_list_url, function(data) {
+			/* Verify that the JSON retrieved is indeed array */
+			if (Object.prototype.toString.call(data) != '[object Array]') {
+				throw 'TypeError: Manifest list retrieved via Drupal manifest list URL is not an JSON array';
+			}
+	
+			/* Move the current page's manifest to the top of the manifest list, so the bookreader loads it first while loading the others in the background afterwards */	
+			var new_data = []; 
+			$.each(data, function(idx, obj) {
+				if (obj.manifestUri == manifest_url) {
+					new_data.unshift(obj); 
+				} else {
+					new_data.push(obj); 
+				}
+			});
+
+			Mirador({
+				"id": "mirador-bookreader",
+				"data": new_data,		
+				'buildPath' : '/sites/all/libraries/mirador/',
+				'i18nPath' : '/sites/all/libraries/mirador/locales/',
+				'imagesPath' : '/sites/all/libraries/mirador/images/',
+				'logosPath' : '/sites/all/libraries/mirador/images/logos/',
+				'logosLocation' : '/sites/all/libraries/mirador/images/logos/',
+				'repoImages' : {
+					'other': 'iiif_logo.png'
+				},
+				"windowObjects": [
+					{
+						"loadedManifest":manifest_url,
+					}
+				]
+			});
+		});
+	};
 });
 
 })(jQuery);
