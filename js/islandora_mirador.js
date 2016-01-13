596ddfa096352c88f04795235c63b7a7c4ab6ad1
Sean Xiao Zhao <xiaofeng.zhao@utoronto.ca>
2016-01-12 19:27:08 -0500

    Fixed typo

--- js/islandora_mirador.js
+++ js/islandora_mirador.js
@@ -20,7 +20,7 @@ $(function() {
 	/* Setup the base URL for windowObjects or data attribute to render */
 	var pid = Drupal.settings.islandora_mirador_bookreader.pid;
 	var base_url = Drupal.settings.islandora_mirador_bookreader.base_url;
-	var maniifest_datastream_id = Drupal.settings.islandora_mirador_bookreader.manifest_datastream_id;
+	var manifest_datastream_id = Drupal.settings.islandora_mirador_bookreader.manifest_datastream_id;
 	var manifest_url = base_url + "/islandora/object/" + pid + "/datastream/" + manifest_datastream_id + "/view";
 	if (!manifest_datastream_id) {
 		/* no datastream specified, which means no image can be loaded. Use a dummy as a placeholder until the datastream is available */
